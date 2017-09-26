d59a47d98415e33eebbe178b357dc8f4a25d1755
mstelmach <Matt.w.stelmach@gmail.com>
2017-09-26 06:56:33 -1000

    static files not running on ubuntu

--- portfolioProj/settings.py
+++ portfolioProj/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'Templates')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -57,7 +59,9 @@ ROOT_URLCONF = 'portfolioProj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [TEMPLATE_DIR,
+                 os.path.join(BASE_DIR, 'home/Templates'),
+                 os.path.join(BASE_DIR, 'work_history/Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
